4d8ca8f860c63bed6aa98b8f459c1162508e80bf
Lidya <Lidya@DESKTOP-B4R0CR6>
2021-03-06 16:13:09 +0100

    Clase git nueva

---
+++ ejercicio5/src/ejercicio5/Redondeo_numero.java
@@ -0,0 +1,17 @@
+package ejercicio5;
+
+public class Redondeo_numero {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		
+		double x;
+		x=5.5;
+		
+		double y= Math.ceil(x) ;
+				
+		System.out.println("El numero " +  x  + " redondeado es " + y );
+	}
+
+}
